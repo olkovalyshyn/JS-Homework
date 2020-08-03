48ca889400ecc005dd01b61b1cd93a9ef9098b9f
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-08-04 01:37:47 +0300

    Вирішує ДЗ Модуль-2 (задачі 4-8)

--- js/script.js
+++ js/script.js
@@ -430,30 +430,30 @@
 // Поиск самого длинного слово в строке с пробелами
 // Напиши функцию findLongestWord(string = ""), которая принимает параметром произвольную строку(в строке будут только слова и пробелы)
 // и возвращает самое длинное слово в этой строке.
-function findLongestWord(string = "") {
-  //   "use strict";
-  // Write code under this line
-  const words = string.split(" ");
-  let lengthLongestWord = words[0].length;
-  let nameLongestWord = words[0];
+// function findLongestWord(string = "") {
+//   //   "use strict";
+//   // Write code under this line
+//   const words = string.split(" ");
+//   let lengthLongestWord = words[0].length;
+//   let nameLongestWord = words[0];
 
-  for (let i = 0; i < words.length; i += 1) {
-    if (lengthLongestWord < words[i].length) {
-      lengthLongestWord = words[i].length;
-    }
-  }
-  for (let i = 0; i < words.length; i += 1) {
-    if (lengthLongestWord === words[i].length) {
-      nameLongestWord = words[i];
-    }
-  }
+//   for (let i = 0; i < words.length; i += 1) {
+//     if (lengthLongestWord < words[i].length) {
+//       lengthLongestWord = words[i].length;
+//     }
+//   }
+//   for (let i = 0; i < words.length; i += 1) {
+//     if (lengthLongestWord === words[i].length) {
+//       nameLongestWord = words[i];
+//     }
+//   }
 
-  return nameLongestWord;
-  console.log(lengthLongestWord);
-  console.log(nameLongestWord);
-}
+//   return nameLongestWord;
+//   console.log(lengthLongestWord);
+//   console.log(nameLongestWord);
+// }
 
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 // 'jumped'
 
 // console.log(findLongestWord("Google do a roll"));
@@ -461,3 +461,277 @@ console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 
 // console.log(findLongestWord("May the force be with you"));
 // 'force'
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Задача 2 - 4
+// Форматирование строки в зависимости от длинны строки
+// Напиши функцию formatString(string, maxLength = 40) которая принимает строку и форматирует ее если необходимо.
+
+// Если длина строки не превышает maxLength, функция возвращает ее в исходном виде.
+// Если длина больше maxLength, то функция обрезает строку до размера maxLength символов и добавляет в конец строки троеточие ...,
+// после чего возвращает укороченную версию.
+// function formatString(string, maxLength = 40) {
+//   // Write code under this line
+
+//   let lengthSentences = string.length;
+
+//   if (lengthSentences <= maxLength) {
+//     return string;
+//   }
+
+//   let arraySentences = string.split("");
+//   arraySentences.length = maxLength;
+//   string = arraySentences.join("") + "...";
+//   return string;
+// }
+//console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30));
+// 'Vestibulum facilisis, purus ne...'
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Задача 2 - 5
+// Функция предикат
+// Функция предикат возвращает true или false
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.Функция
+// проверяет ее на содержание слов spam и sale.Если нашли запрещенное слово то функция
+// возвращает true, если запрещенных слов нет функция возвращает false.Слова в строке
+// могут быть в произвольном регистре.
+// function checkForSpam(str) {
+//   "use strict";
+//   // Write code under this line
+//   let onLowerCase = "";
+//   onLowerCase = str.toLowerCase();
+//   const message = onLowerCase.includes("spam") || onLowerCase.includes("sale") ? true : false;
+//   return message;
+// }
+
+//console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+
+//console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Задача 2 - 6
+// Создание нового массива на основе преобразования данных из старого массива
+// Напиши функцию mapArray(array), принимающую 1 параметр array - массив чисел.
+// Функция создает новый массив numbers размером в длину массива array и заполняет
+// его числами из массива array умноженными на 10. Для перебора массива и добавления
+// новых значений используй цикл for.По завершению перебора массива array возвращается
+// массив numbers.
+// function mapArray(array) {
+//   "use strict";
+//   const numbers = new Array(array.length);
+//   for (let i = 0; i < array.length; i += 1) {
+//     // Write code under this line
+//     numbers[i] = array[i] * 10;
+//   }
+//   return numbers;
+// }
+
+// console.log(mapArray([-2, 0, 2]));
+// [-20, 0, 20]
+
+// console.log(mapArray([-2.5, 0, 2.5]));
+// [-25, 0, 25]
+
+// Задача 2 - 7
+// Создание нового массива на основе отбора из исходного массива
+// Напиши функцию filterArray(array), принимающую 1 параметр array - массив.
+// Функция проверяет каждое значение массива - является ли значение конечным
+// числом(Number.isFinite).Для проверки используй цикл for.Числа добавляются
+// в новый массив numbers с помощью метода push, а не числа - игнорируются.По
+// завершению проверки массива array возвращается массив numbers.
+// function filterArray(array) {
+//   "use strict";
+//   let numbers = [];
+//   for (let i = 0; i < array.length; i += 1) {
+//     // Write code under this line
+//     if (Number.isFinite(array[i]) === false) {
+//       continue;
+//     }
+//     numbers.push(array[i]);
+//   }
+
+//   return numbers;
+// }
+
+// console.log(filterArray([-2, 0, 2]));
+// [-2, 0, 2]
+
+// console.log(filterArray([1, NaN, Infinity]));
+// [1]
+
+// console.log(filterArray([0, -0, 100, "100"]));
+// [0, 0, 100]
+
+// console.log(filterArray([undefined, false, null, [], 1]));
+// [1]
+
+// console.log(filterArray([{}, () => {}, 2]));
+// [2]
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Задача 2 - 8
+// Вычисление суммы массива
+// Напиши функцию reduceArray(array), которая принимает числовой массив array.
+// Если массив array не пустой, необходимо посчитать сумму всех элементов массива,
+//     используя цикл for.Для подсчета суммы используй переменную total.
+// Функция должна возвращать 0 если массив пустой и значение total в противном случае.
+// function reduceArray(array) {
+//   "use strict";
+//   let total = 0;
+//   // Write code under this line
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (array.length === 0) {
+//       return total;
+//     }
+//     total += array[i];
+//   }
+//   return total;
+// }
+
+// console.log(reduceArray([1, 2, 3]));
+// 6
+
+//console.log(reduceArray([-2, 0, 2]));
+// 0
+
+// console.log(reduceArray([1, 2, 2.5]));
+// 5.5
+
+// Задача 2 - 9
+// Есть массив logins с логинами пользователей.Напиши скрипт
+// добавления логина в массив logins.Добавляемый логин должен:
+
+// проходить проверку на длину от 4 до 16 - ти символов включительно
+// быть уникален, то есть отсутствовать в массиве logins
+// Разбей задачу на подзадачи с помощью функций.
+// Проверку на отсутствие аргументов или на правильный тип аргументов делать не нужно.
+
+// Напиши функцию isLoginValid(login), в которой проверь количество символов параметра
+// login и верни true или false в зависимости от того, попадает ли длина параметра в заданный диапазон от 4 - х до 16 - ти символов включительно.
+
+// Напиши функцию isLoginUnique(allLogins, login), которая принимает список всех логинов
+// и добавляемый логин как параметры и проверяет наличие login в массиве allLogins, возвращая
+// true если такого логина еще нет и false если логин уже используется.
+
+// Напиши функцию addLogin(allLogins, login) которая:
+
+// Принимает новый логин и массив всех логинов как параметры
+// Проверяет валидность логина используя вспомогательную функцию isLoginValid
+// Если логин не валиден, прекратить исполнение функции addLogin и вернуть строку 'Ошибка! Логин должен быть от 4 до 16 символов'
+// Если логин валиден, функция addLogin проверяет уникальность логина с помощью функции isLoginUnique
+// Если isLoginUnique вернет false, тогда addLogin не добавляет логин в массив и возвращает строку 'Такой логин уже используется!'
+// Если isLoginUnique вернет true, addLogin добавляет новый логин в logins и возвращает строку 'Логин успешно добавлен!'
+
+// 🔔 Принцип единственной ответственности функции - каждая функция делает что - то одно.Это позволяет переиспользовать
+// код и изменять логику работы функции только в одном месте, не затрагивая работу программы в целом.
+
+// Предикатные функции возвращают только true или false.Такие функции принято называть начиная с is: isLoginUnique и isLoginValid в нашем случае.
+
+// isLoginUnique только проверяет есть ли такой логин в массиве и возвращает true или false.
+// isLoginValid только проверяет валидный ли логин и возвращает true или false.
+// addLogin добавляет или не добавляет логин в массив.При этом для проверок условия добавления использует результаты вызовов
+// других функций - isLoginUnique и isLoginValid.
+
+// function isLoginValid(login, min = 4, max = 16) {
+//   // Write code under this line
+//   let messageIsLoginValid = "Ошибка! Логин должен быть от 4 до 16 символов";
+//   if ((login.length >= min && login.length <= max) === true) {
+//     return true;
+//   }
+//   return messageIsLoginValid;
+// }
+
+function isLoginUnique(allLogins, login) {
+  "use strict";
+  // Write code under this line
+  let messageisLoginUnique = "Такой логин уже используется!";
+  //   allLogins.includes(login);
+  if (allLogins.includes(login) === true) {
+    return messageisLoginUnique;
+  }
+}
+
+function addLogin(allLogins, login) {
+  "use strict";
+  const SUCCESS = "Логин успешно добавлен!";
+  const REFUSAL = "Такой логин уже используется!";
+  const ERROR = "Ошибка! Логин должен быть размером от 4 до 16 символов";
+  let message;
+  // Write code under this line
+    
+  function isLoginValid(login);
+  function isLoginUnique(allLogins, login);
+}
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+console.log(addLogin(logins, "Ajax"));
+// 'Логин успешно добавлен!'
+
+// console.log(addLogin(logins, 'robotGoogles'));
+// 'Такой логин уже используется!'
+
+// console.log(addLogin(logins, 'Zod'));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+// console.log(addLogin(logins, 'jqueryisextremelyfast'));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+// console.log(isLoginValid("Ajww"));
+
+console.log(isLoginUnique(logins, "Mango1"));
