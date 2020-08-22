0b80294ee15c059d1d909c43925373f966dbe65e
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-08-23 01:35:53 +0300

    Добавляє приклади прототипного наслідування

--- js/script.js
+++ js/script.js
@@ -86,28 +86,69 @@
 //
 //
 // ДЕСТРУКТКРИЗАЦІЯ (вдосконалення вищеописаної) Ф-ЦІЇ (за вищеописаною задачею)
-const Car = function ({ brand, model, prise } = {}) {
-  this.brand = brand;
-  this.model = model;
-  this.prise = prise;
+// const Car = function ({ brand, model, prise } = {}) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.prise = prise;
 
-  this.changePrise = function (newPrise) {
-    this.prise = newPrise;
-  };
-};
-console.log(Car.prototype);
+//   // this.changePrise = function (newPrise) {
+//   //   this.prise = newPrise;
+//   // };
+// };
+// console.log(Car.prototype);
 
-Car.prototype.sayHi = function () {
-  console.log("Car.prototype.sayHi -> this: ", this);
-  console.log("Hellow )))");
-};
+// Car.prototype.sayHi = function () {
+//   console.log("Car.prototype.sayHi -> this: ", this);
+//   console.log("Hellow )))");
+// };
 
-const myCar1 = new Car({ brand: "Mersedes", model: "BENZ", prise: 5000 });
-myCar1.sayHi();
+// Car.prototype.changeBrand = function (newBrand) {
+//   this.brand = newBrand;
+// };
+
+// Car.prototype.changePRISE = function (newPrise) {
+//   this.prise = newPrise;
+// };
+
+// const myCar1 = new Car({ brand: "Mersedes", model: "BENZ", prise: 5000 });
+// myCar1.sayHi();
+// console.log(myCar1);
+
+// myCar1.changeBrand("VAZ");
 // console.log(myCar1);
 
 // const myCar2 = new Car({ brand: "BMW", model: "X5", prise: 40000 });
 // console.log(myCar2);
 
-// const myCar3 = new Car();
-// console.log(myCar3);
+// myCar2.changeBrand("LADA");
+// myCar2.changePRISE(1000000000000);
+// console.log("myCar2 = ", myCar2);
+
+// // const myCar3 = new Car();
+// // console.log(myCar3);
+//
+//
+//
+//
+//
+//
+//
+//
+// const Some = function (value) {
+//   this.x = value;
+// };
+
+// const objA = new Some();
+
+// objA.x = 555;
+// console.log(objA);
+
+// // console.log(objA.__proto__ === Object.prototype);
+// // console.log(objA);
+
+// Some.prototype.sometingChange = function (change) {
+//   this.x = change;
+// };
+// objA.sometingChange(98765);
+
+// console.log(objA);
